a89c8094361fb9b1bf29cbafbc60e1e33287246e
Mezz <mezzmonteur@gmail.com>
2026-07-29 17:10:34 +0200

    Add import for recordVisit from analytics.js

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+import { recordVisit } from './analytics.js';
 
 
 /* ================================================================
