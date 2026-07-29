fc5e888d3dc494a2bccb4c4e00157aaca4eeb7e9
Mezz <mezzmonteur@gmail.com>
2026-07-29 18:55:52 +0200

    Remove unused import of recordVisit
    Removed unused import for recordVisit from analytics-store.js

--- js/script.js
+++ js/script.js
@@ -25,8 +25,6 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-import { recordVisit } from 'js/analytics-store.js'; 
-
 
 /* ================================================================
    2. CONFIGURATION GLOBALE & ÉTAT PARTAGÉ
